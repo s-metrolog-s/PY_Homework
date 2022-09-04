69df01e42caadfaedb57dfee3c90b7cd9567e242
Sergey <metrolog_omsk@mail.ru>
2022-09-05 00:28:08 +0500

    Набор произведений от 1 до N

--- Task_202.py
+++ Task_202.py
@@ -1,3 +1,14 @@
 # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+n = int(input('Введите число N: '))
+
+list_numbers = []
+for i in range(1, n + 1):
+    result = 1
+    for k in range(1, i + 1):
+        result *= k
+    list_numbers.append(result)
+
+print(list_numbers)
