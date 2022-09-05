42b3921dd3552f5a4ddb5e7c9dd00f2ffc85d499
Sergey <metrolog_omsk@mail.ru>
2022-09-05 11:34:08 +0500

    Алгоритм перемешивания списка

--- Task_205.py
+++ Task_205.py
@@ -1,1 +1,15 @@
-# Реализуйте алгоритм перемешивания списка.
+# Реализуйте алгоритм перемешивания списка
+from random import randint
+
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+mix_list = []
+print(f'Дан список: {list_1}')
+print('Перемешаем его как следует...')
+
+# перемешивание списка через Random
+while list_1 != []:
+    item = randint(0, len(list_1)-1)
+    mix_list.append(list_1.pop(item))
+
+print(f'Готовый список: {mix_list}')
+
