6a7be846aaff196c0d91468b3dcf2d0aa8636bde
Sergey <metrolog_omsk@mail.ru>
2022-09-05 10:51:22 +0500

    Задание списка по N и произведение элементов по индексу

--- Task_204.py
+++ Task_204.py
@@ -1,2 +1,18 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных пользователем через пробел позициях.
+
+n = int(input('Введите число N: '))
+list_numbers = []
+mult_list = []
+
+for i in range(-n, n + 1):
+    list_numbers.append(i)
+print(list_numbers)
+
+mult_numbers = input('Введите номера элементов списка для умножения через пробел: ')
+mult_list = mult_numbers.split(' ')
+
+# Ввожу -1 у значений индексов для удобства пользователя, при счете с 1
+result = list_numbers[int(mult_list[0]) - 1] * list_numbers[int(mult_list[1]) - 1]
+
+print(result)
